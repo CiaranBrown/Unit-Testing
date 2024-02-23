0dff02def2988d1de391484a373b93bae76c0081
Ciaran Brown <ciaranbrown@Ciarans-Air.lan>
2024-02-23 10:58:05 +0000

    Workshop complete

--- learn-unit-testing/workshop/index.js
+++ learn-unit-testing/workshop/index.js
@@ -8,3 +8,12 @@ function searchParamsToObject(string) {
   const params = new URLSearchParams(string);
   return Object.fromEntries(params);
 }
+
+function isLeapYear(year) {
+  if (year % 4 === 0) return true;
+  if (year % 100 === 0) return false;
+  if (year % 400 === 0) return true;
+  if (typeof year !== "number") return "Please enter a number";
+  if (year < 0) return "Year cannot be negative";
+  return false;
+};
--- learn-unit-testing/workshop/index.test.js
+++ learn-unit-testing/workshop/index.test.js
@@ -10,3 +10,23 @@ test("searchParamsToObject will create an object from a string", (t) => {
     equal(actual.email, expected.email);
 });
 
+test("isLeapYear should be invalid input", () => {
+    equal(isLeapYear("hi"), "Please enter a number", "Input must be a number");
+    equal(isLeapYear(-1), "Year cannot be negative", "Input must be positive");
+    equal(isLeapYear(2023), false, "2023 should not be a leap year");
+  });
+  
+  test("isLeapYear should handle multiples of 4", () => {
+    equal(isLeapYear(2020), true, "2020 should be a leap year");
+    equal(isLeapYear(2024), true, "2024 should be a leap year");
+  });
+  
+  test("isLeapYear should handle multiples of 100", () => {
+    equal(isLeapYear(1800), false, "1800 should not be a leap year");
+    equal(isLeapYear(300), false, "300 should not be a leap year");
+  });
+  
+  test("isLeapYear should handle multiples of 400", () => {
+    equal(isLeapYear(2000), true, "2000 should be a leap year");
+    equal(isLeapYear(1600), true, "1600 should be a leap year");
+  });
